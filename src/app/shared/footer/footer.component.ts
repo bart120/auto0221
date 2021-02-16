fd1fa5755cc7041619ab355c59c49be78bdf9f34
Vincent <vleclerc@inow.fr>
2021-02-16 14:53:51 +0100

    unsubscribe

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { MarkModel } from 'src/app/models/mark.model';
 import { MarkService } from 'src/app/services/mark.service';
 
 @Component({
@@ -8,6 +10,8 @@ import { MarkService } from 'src/app/services/mark.service';
 export class FooterComponent implements OnInit {
 
     marks: Array<any> = [];
+
+    subscripton?: Subscription;
 
     //private servMark: MarkService;
 
@@ -22,10 +26,20 @@ export class FooterComponent implements OnInit {
             { name: 'Audi', image: 'audi.jpg' }
         ];*/
         //this.marks = this.servMark.getMarks();
-        this.servMark.getMarksRepeatOrderByName().subscribe(data => {
+        this.subscripton = this.servMark.getMarksRepeatOrderByName().subscribe(data => {
             this.marks = data;
         },
             err => { console.warn(err); },
             () => { console.log('complete') });
+    }
+
+    ngOnDestroy(): void {
+        //Called once, before the instance is destroyed.
+        //Add 'implements OnDestroy' to the class.
+        this.subscripton?.unsubscribe();
+    }
+
+    stop(): void {
+        this.subscripton?.unsubscribe();
     }
 }
