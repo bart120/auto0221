172ed62062864079154ff3c75d099acb38da998a
Vincent <vleclerc@inow.fr>
2021-02-16 16:09:00 +0100

    localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { LOCALE_ID, Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'auto0221';
+
+  constructor(@Inject(LOCALE_ID) private locale: string) {
+
+  }
+
+  change(): void {
+    this.locale = 'es';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -7,6 +7,13 @@ import { SharedModule } from './shared/shared.module';
 import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+
+import localeFr from '@angular/common/locales/fr';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeEs, 'es');
 
 @NgModule({
   declarations: [
@@ -20,7 +27,7 @@ import { HttpClientModule } from '@angular/common/http';
     SharedModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
