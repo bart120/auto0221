ccdebdf1281424c36e38a8f14ed2dc96a1c5498b
Vincent <vleclerc@inow.fr>
2021-02-16 09:19:25 +0100

    redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { LoginComponent } from './authentication/login/login.component';
 import { AddComponent } from './car/add/add.component';
 import { ListComponent } from './car/list/list.component';
 import { HomeComponent } from './master/home/home.component';
+import { NotfoundComponent } from './master/notfound/notfound.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'car/list', component: ListComponent },
     { path: 'car/add', component: AddComponent },
-    { path: 'auth/login', component: LoginComponent }
+    { path: 'auth/login', component: LoginComponent },
+
+    { path: '404', component: NotfoundComponent },
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+    { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
--- src/app/master/master.module.ts
+++ src/app/master/master.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NotfoundComponent],
   imports: [
     CommonModule
   ]
